c2dd9a713ee12bcad1ebb00e15a7937b1fdb7669
aaryan11-hash <aaryan2000dpspune@gmail.com>
2020-07-11 15:47:03 +0530

    implemented apache commons csv dependancy to parse through the string csv data ends #2

--- src/main/java/com/aaryan/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/aaryan/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,10 +1,13 @@
 package com.aaryan.coronavirustracker.services;
 
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -25,7 +28,16 @@ public class CoronaVirusDataService {
                 .build();
 
         HttpResponse<String> httpResponse=client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader=new StringReader(httpResponse.body());
+
+        Iterable<CSVRecord> records= CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        for(CSVRecord record:records){
+            String state=record.get("Province/State");
+            System.out.println(state);
+
+
+        }
 
     }
 
